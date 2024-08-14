c678caf999f93662fa7c5228368c667ba59e8084
supermax.dnepr@gmail.com <supermax.dnepr@gmail.com>
2024-08-14 14:22:06 +0300

    Add project files.

---
+++ Internship Task/Program.cs
@@ -0,0 +1,40 @@
+using Internship_Task.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+
+namespace Internship_Task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+            builder.Services.AddSingleton<EmailValidator>();
+            builder.Services.AddSingleton<User>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
